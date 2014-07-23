f0962a2dc211e6e59c22bdebdfc3334aa1082ef7
Raghu Betina <git@rugb.com>
2014-07-23 11:16:59 -0500

    Added comments to Photos#show page, validations to comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ActiveRecord::Base
+  validates :content, :presence => true
+  validates :user_id, :presence => true
+  validates :photo_id, :presence => true
+
   # belongs to one photo
   def photo
     return Photo.find_by({ :id => self.photo_id })
