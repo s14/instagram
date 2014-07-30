a4a0198cc4adce81e99761883510f5877d30e24b
Raghu Betina <git@rugb.com>
2014-07-30 12:34:18 -0500

    Removed URL helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,22 +3,23 @@ module ApplicationHelper
     return "<h1>Howdy #{person}!</h1>".html_safe
   end
 
-  # Returns the URL for the show page of a photo
+  # Define a method that returns a string like: '/photos/new'
+  def new_photo_url
+
+  end
+
+  # Define a method that returns a string like: '/photos'
+  def photos_url
+
+  end
+
+  # Define a method that returns a string like: '/photos/:id'
   def photo_url(object)
-    if object.class == Photo
-      return "/photos/#{object.id}"
-    else
-      return "/photos/#{object}"
-    end
+
   end
 
-  # Returns the URL for the edit page of a photo
+  # Define a method that returns a string like: '/photos/:id/edit'
   def edit_photo_url(object)
-    return "/photos/#{object.id}/edit"
-  end
 
-  # Returns the URL to delete a photo
-  def delete_photo_url(object)
-    return "/photos/#{object.id}"
   end
 end
